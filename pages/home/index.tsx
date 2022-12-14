af744104eadb372b36654535281e5aaecf07b883
phuoc-dev <phuoc.trung.nguyen.96@gmail.com>
2022-12-15 03:07:03 +0700

    updated the component paths

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,7 +1,7 @@
-import Footer from '../../components/Footer';
-import HowItWorks from '../../components/home/HowItWorks';
-import FAQs from '../../components/home/FAQs';
-import HeadSection from '../../components/home/HeadSection';
+import Footer from '@/components/Footer';
+import HowItWorks from '@/components/home/HowItWorks';
+import FAQs from '@/components/home/FAQs';
+import HeadSection from '@/components/home/HeadSection';
 
 const Home = () => {
   return (
