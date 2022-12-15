b0805d954444eb65ce4cee78ee0d8faf2c9b0c09
It-Superdev <105212989+It-SuperDev@users.noreply.github.com>
2022-12-16 00:33:27 +0600

    Fix modal background

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -16,7 +16,7 @@ const Modal = ({ open, onClose, children }: any) => {
         {open ? (
           <>
             <div className="fixed -ml-10 -mt-10 inset-10 bg-[#00000052] overflow-y-auto h-full w-full" />
-            <div className="fixed max-[768px]:bottom-0 md:top-[50%] left-[50%] -translate-x-2/4 md:-translate-y-2/4 mx-auto md:p-8 p-4 md:pb-8 pb-7 md:w-[578px] w-full shadow-lg md:rounded-lg rounded-tl-[24px] rounded-tr-[24px] rounded-bl-0 rounded-br-0 bg-white bg-gradient-to-b from-modal-from/25 via-modal-via/10 to-white">
+            <div className="fixed max-[768px]:bottom-0 md:top-[50%] left-[50%] -translate-x-2/4 md:-translate-y-2/4 mx-auto md:p-8 p-4 md:pb-8 pb-7 md:w-[578px] w-full shadow-lg md:rounded-lg rounded-tl-[24px] rounded-tr-[24px] rounded-bl-0 rounded-br-0 bg-white modal">
               <span
                 onClick={() => onClose()}
                 className="md:w-6 w-8 md:h-6 h-8 absolute md:-top-[18px] top-[8px] md:-right-[18px] right-[8px] cursor-pointer"
